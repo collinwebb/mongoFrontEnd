ca7e9ea240a61fb5c589c9989d7fc9363f52e155
Collin Webb <collindwebb@gmail.com>
2015-07-04 09:57:00 -0700

    makes answer mockup

--- script.js
+++ script.js
@@ -1,6 +1,75 @@
-'use strict';
+angular.module('Mongo', ['ui.router'])
+.constant('ATN', {
+  // 'API_URL': 'http://localhost:3000/',
+  API_URL: 'https://mongoquestions.herokuapp.com/',
+})
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/");
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "list.html",
+      controller: 'MainCtrl'
+    })
+    .state('new', {
+      url: "/new",
+      templateUrl: "new.html",
+      controller: "NewQuestionCtrl"
+    })
+    .state('404', {
+      url: "/404",
+      templateUrl: "404.html",
+    })
+    .state('question', {
+      url: "/:slug",
+      templateUrl: "question.html",
+      controller: "QuestionCtrl"
+    });
+})
+.factory('Question', function($http, ATN){
+  return {
+    getOne: function(slug){
+      return $http.get(ATN.API_URL + 'questions/' + slug);
+    },
+    getAll: function(){
+      return $http.get(ATN.API_URL + 'questions');
+    },
+    addQuestion: function(newQuestion){
+      return $http.post(ATN.API_URL + 'questions', newQuestion);
+    },
+  };
+})
+.filter('dateInWords', ['$http', function(){
+  return function(input){
+    return moment(input).utc().fromNow();
+  };
+}])
+.controller('QuestionCtrl', function($scope, Question, $state){
+  $scope.slug = $state.params.slug;
 
-angular.module('heimdall', [])
-
-.controller('MainCtrl', function($scope){
+  Question.getOne($state.params.slug)
+    .success(function(data) {
+      $scope.question = data;
+    }).catch(function(err) {
+      $state.go('404');
+      console.error(err);
+    });
+})
+.controller('MainCtrl', function($scope, $http, Question){
+  Question.getAll().success(function(data){
+    $scope.questions = data;
+  }).catch(function(error){
+    console.error(error);
+  });
+})
+.controller('NewQuestionCtrl', function($scope, $state, Question){
+  $scope.askQuestion = function(){
+    Question.addQuestion($scope.ask)
+    .success(function(data){
+      $state.go('home');
+    })
+    .catch(function(data){
+      console.error(data);
+    });
+  };
 });
